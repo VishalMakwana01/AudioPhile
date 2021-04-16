e61f0ae816a0de9de1dcb78c2fd0d10809abae23
YashJaveri <yjaveri99@gmail.com>
2021-04-16 17:45:53 +0530

    Initial Commit

--- AudiophileDjango/BEProj/views.py
+++ AudiophileDjango/BEProj/views.py
@@ -3,6 +3,10 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 import io
+import numpy as np
+from scipy.io.wavfile import write
+import torch
+
 
 class AudioBook(APIView):
 
@@ -20,6 +24,33 @@ class AudioBook(APIView):
             content += text.extractText()
             i +=1
         print(content)
-        #text to audio
+
+        tacotron2 = torch.hub.load('nvidia/DeepLearningExamples:torchhub', 'nvidia_tacotron2')
+        tacotron2 = tacotron2.to('cuda')
+        tacotron2.eval()
+
+        melgan = torch.hub.load('seungwonpark/melgan', 'melgan')
+        melgan.eval()
+
+        sequence = np.array(tacotron2.text_to_sequence(content, ['english_cleaners']))[None, :]
+        sequence = torch.from_numpy(sequence).to(device='cuda', dtype=torch.int64)
+
+        with torch.no_grad():
+            _, mel, _, _ = tacotron2.infer(sequence)
+
+            if torch.cuda.is_available():
+                melgan = melgan.cuda()
+                mel = mel.cuda()
+
+            # print(mel.shape)
+            audio = melgan.inference(mel)
+
+        audio_numpy = audio.cpu().detach().numpy()
+        rate = 22050
+
+        write("audio.wav", rate, audio_numpy)
+
+        from IPython.display import Audio
+        Audio(audio_numpy, rate=rate)
 
         return Response()
